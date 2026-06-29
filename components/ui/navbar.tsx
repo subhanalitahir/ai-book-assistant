508e966cdb72f9fab459621364ac24497f67b296
Subhan Ali <subhanalitahir.dev@gmail.com>
2026-06-29 18:55:22 +0500

    Fixed some issues

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   const { user } = useUser();
   return (
     <div>
-      <header className="w-full fixed z-50 bg('--bg-primary')">
+      <header className="w-full fixed z-50 bg-[var(--bg-primary)]">
         <div className="wrapper navbar-height py-4 flex justify-between items-center">
           <Link href={"/"} className="flex gap-0.5 items-center">
             <Image
